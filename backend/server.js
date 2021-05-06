1751948a9f6bd3683393e2d6fe632dc43fd823cb
Mihail Tudos <mihailtudos@outlook.com>
2021-05-06 14:12:49 +0100

    added env file and some setings

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 })
